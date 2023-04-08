847018233981e51373f87b3aa69f200aabaef29d
HuyNguyenUTA <huy.nguyen.uta@gmail.com>
2023-04-07 19:43:59 -0500

    created input box

--- mindsift/pages/index.tsx
+++ mindsift/pages/index.tsx
@@ -1,27 +1,11 @@
-import Head from 'next/head';
-import React from 'react';
-import { Inter } from 'next/font/google';
-import { Style } from 'util';
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
-const styles = {
-  background: {
-    height: '100%'
-  },
-  inputBox: {
-    display: 'flex',
-    width: '50px',
-    height: '100%',
-    backgroundColor: '#777777'
-  }
-}
-
-interface HomeProps {
-  styles: typeof styles; // declare the type of styles prop
-}
-
-const Home: React.FC<HomeProps> = ({styles}) => {
+export default function Home() {
   return (
     <>
       <Head>
@@ -30,14 +14,12 @@ const Home: React.FC<HomeProps> = ({styles}) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <body>
-          <div style={styles.inputBox}>
-          </div>
-        </body>
+      <main className={styles.main}>
+        <div className={styles.inputWrapper}>
+          <label className={styles.input}>What&apos;s on your mind ?</label>
+          <input className={styles.input} type="text" placeholder="Typing ..."/>
+        </div>
       </main>
     </>
-  );
+  )
 }
-
-export default Home;
